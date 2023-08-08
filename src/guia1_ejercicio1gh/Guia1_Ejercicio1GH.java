3822d4f9c86ccab8bd6dbd198196563d043df69d
GabiiR <grgithub@gmail.com>
2023-08-08 19:46:30 -0300

    Initial commit

---
+++ src/guia1_ejercicio1gh/Guia1_Ejercicio1GH.java
@@ -0,0 +1,8 @@
+package guia1_ejercicio1gh;
+
+public class Guia1_Ejercicio1GH {
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/guia1_ejercicio1gh/Perro.java
@@ -0,0 +1,6 @@
+package guia1_ejercicio1gh;
+
+public class Perro {
+    private String nombre;
+    private int edad;
+}
